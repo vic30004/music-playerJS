4d20e7f02a827a55a96a743b16e066fe55f2464f
vic30004 <victor_993@msn.com>
2020-04-13 20:12:38 -0700

    Added a function to move to next song once the song is over

--- app.js
+++ app.js
@@ -96,3 +96,6 @@ audio.addEventListener('timeupdate', updateProgress);
 
 // Click on progress bar
 progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended',nextSong)
