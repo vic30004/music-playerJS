4a04605ff822bb6058d60d87f27b07e61d0d2d04
vic30004 <victor_993@msn.com>
2020-04-13 19:42:33 -0700

    added all the necessary dom variables

---
+++ app.js
@@ -0,0 +1,14 @@
+const musicContainer = document.getElementById('music-container');
+
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progressContainer');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
