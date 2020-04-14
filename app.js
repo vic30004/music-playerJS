104400886ea7362b6f3f12bf6a1ab2b91d7d01d3
vic30004 <victor_993@msn.com>
2020-04-13 19:46:56 -0700

    added a function to load the song info

--- app.js
+++ app.js
@@ -12,3 +12,16 @@ const cover = document.getElementById('cover');
 
 // Song titles
 const songs = ['hey', 'summer', 'ukulele'];
+
+// Keep track of song
+let songIndex = 2;
+
+// Initially load song deatils into DOM
+loadSong(songs[songIndex]);
+
+// Update song details
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `./music/${song}.mp3`;
+    cover.src = `./images/${song}.jpg`;
+}
